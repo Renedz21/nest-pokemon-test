2f2ca3984e91570bb98e3acac9f799af79ce1c68
Renedz21 <edzonperez.castillo@gmail.com>
2022-10-23 17:59:31 -0500

    add serve static

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonModule } from './pokemon/pokemon.module';
@@ -6,12 +7,16 @@ import { SeedModule } from './seed/seed.module';
 import { ConfigModule } from '@nestjs/config';
 import { EnvConfig } from './config/app.config';
 import { JoiValidationSchema } from './config/joi.validation';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [EnvConfig],
       validationSchema: JoiValidationSchema
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
     }),
     MongooseModule.forRoot(process.env.MONGODB),
     PokemonModule,
